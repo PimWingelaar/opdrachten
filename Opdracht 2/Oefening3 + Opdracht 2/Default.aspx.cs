2b629fb3f3c60f3a86acafbfe171ff59117f7085
Pim Wingelaar <pwingelaar01@student.rocvantwente.nl>
2017-02-13 10:24:53 +0100

    Opdracht 1 en Opdracht 2 toegevoegt bij GitHub

---
+++ Opdracht 2/Oefening3 + Opdracht 2/Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    { }
+    protected void btnMaakKaart_Click(object sender, EventArgs e)
+    {
+        // Het maken van de variabelen die ik ga gebruiken.
+        string Naam, Geslacht, Opleiding, Hobby;
+        // Naam wordt de inhoud van de textbox van de id txtNaam.
+        Naam = txtNaam.Text;
+        // Geslacht wordt gekozen door de gekozen rondje van de id rbGeslacht.
+        Geslacht = rbGeslacht.SelectedValue;
+        // Opleiding wordt gekozen door de gekozen vierkantje van de id cbOpleiding.
+        Opleiding = cbOpleiding.SelectedValue;
+        // Hobby wordt gekozen door de value 1(Gamen of 2(Programmeren) en wordt een zin van gemaakt.
+        Hobby = "Hobby: ";
+        if (chkHobby.SelectedValue == "1")
+        {
+            Hobby += "Gamen ";
+        }
+        if (chkHobby.SelectedValue == "2")
+        {
+            Hobby += "Programmeren ";
+        }
+        // Hier wordt de infomatie geschreven die je hebt ingevoerd.
+        Response.Write("Naam: " + Naam + "<br />");
+        Response.Write(Hobby + "<br />");
+        Response.Write("Opleiding: " + Opleiding + "<br />");
+        Response.Write("Geslacht: " + Geslacht + "<br />");
+    }
+
+
+    protected void rbBeslist_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (rbBeslist.SelectedValue == "1")
+        {
+            pnlopdracht.Visible = true;
+        }
+        if (rbBeslist.SelectedValue == "2")
+        {
+            pnlopdracht.Visible = false;
+        }
+    }
+}
