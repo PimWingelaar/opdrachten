a01c4842d8a19674d6bf9f8f562d9e130d7fcc8d
Pim Wingelaar <pwingelaar01@student.rocvantwente.nl>
2017-04-18 13:36:20 +0200

    Opdracht 7 update

--- Opdracht 7/Default.aspx.cs
+++ Opdracht 7/Default.aspx.cs
@@ -7,6 +7,8 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    private int beoordeling = 0;
+
     protected void Page_Load(object sender, EventArgs e)
     {
     }
@@ -16,18 +18,12 @@ public partial class _Default : System.Web.UI.Page
 
 
         int max = Int32.Parse(txtMax.Text);
-        int[] cijfers = new int[5];
         Random generator = new Random();
-        for (int i = 0; i < 5; i++)
+
+        foreach (Label l in this.Controls[3].Controls.OfType<Label>().Where(label => label.ID.Contains("som")))
         {
-            int Getal = generator.Next(max) + 1;
-            cijfers[i] = Getal;
+            l.Text = Convert.ToString(generator.Next(max) + 1);
         }
-        som1.Text = Convert.ToString(cijfers[0]);
-        som2.Text = Convert.ToString(cijfers[1]);
-        som3.Text = Convert.ToString(cijfers[2]);
-        som4.Text = Convert.ToString(cijfers[3]);
-        som5.Text = Convert.ToString(cijfers[4]);
     }
 
 
@@ -39,43 +35,28 @@ public partial class _Default : System.Web.UI.Page
         int getal3 = Int32.Parse(som3.Text);
         int getal4 = Int32.Parse(som4.Text);
         int getal5 = Int32.Parse(som5.Text);
-        int beoordeling = 0;
 
-        if (getal1 * 1 == Int32.Parse(txtantwoord1.Text))
-        {
-            lbl1.Text = "Goed";
-            txtantwoord1.ReadOnly = true;
-            beoordeling++;
-        } else { lbl1.Text = "Fout"; }
-        if (getal2 * 2 == Int32.Parse(txtantwoord2.Text))
-        {
-            lbl2.Text = "Goed";
-            txtantwoord2.ReadOnly = true;
-            beoordeling++;
-        }
-        else { lbl2.Text = "Fout"; }
-        if (getal3 * 3 == Int32.Parse(txtantwoord3.Text))
-        {
-            lbl3.Text = "Goed";
-            txtantwoord3.ReadOnly = true;
-            beoordeling++;
-        }
-        else { lbl3.Text = "Fout"; }
-        if (getal4 * 4 == Int32.Parse(txtantwoord4.Text))
-        {
-            lbl4.Text = "Goed";
-            txtantwoord4.ReadOnly = true;
-            beoordeling++;
-        }
-        else { lbl4.Text = "Fout"; }
-        if (getal5 * 5 == Int32.Parse(txtantwoord5.Text))
-        {
-            lbl5.Text = "Goed";
-            txtantwoord5.ReadOnly = true;
-            beoordeling++;
-        }
-        else { lbl5.Text = "Fout"; }
+        processDate(getal1, 1, lbl1, txtantwoord1);
+        processDate(getal2, 2, lbl2, txtantwoord2);
+        processDate(getal3, 3, lbl3, txtantwoord3);
+        processDate(getal4, 4, lbl4, txtantwoord4);
+        processDate(getal5, 5, lbl5, txtantwoord5);
+
         lblcijfer.Text = "Uw cijfer is : " + Convert.ToString(beoordeling * 2);
+    }
+
+    private void processDate(int cijfer, int x, Label label, TextBox field)
+    {
+        if (cijfer * x == Int32.Parse(field.Text))
+        {
+            label.Text = "Goed";
+            field.ReadOnly = true;
+            beoordeling++;
+        }
+        else
+        {
+            label.Text = "Fout";
+      }
     }
 
     protected void btnrefresh_Click(object sender, EventArgs e)
